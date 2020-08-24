17449fe2f334c68275d6b7a4c3ee471db6e97a22
Veniamin <Weniaminwelichko@ukr.net>
2020-08-24 17:41:04 +0300

    Completed side task

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,46 +1,68 @@
 'use strict'
 
-const user = {
-    name: 'Test',
-    DateOfBirth: new Date(2000, 1, 6),
-    hoursWorked: 34,
-    ratePerHour: 10,
 
-    get salory(){
-        return this.ratePerHour * this.hoursWorked;
-    },
+1)Нужно написать функцию, принимающую строку в качестве аргумента и возвращающую количество гласных, которые содержатся в строке.
+Гласными являются «a», «e», «i», «o», «u».
+строки - итерируемые. 
+*/
 
-    get age(){
-        let currentDate = new Date();
+//task 1
+function countVowelsV2(string) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  return string.split("").filter((letter) => vowels.includes(letter)).length;
+}
 
-        let correctYear = currentDate.getUTCFullYear();
-        let correctMonth = currentDate.getUTCMonth();
-        let correctDay = currentDate.getUTCDay();
-
-        let userBirthYear = this.DateOfBirth.getFullYear();
-        let userBirthMonth = this.DateOfBirth.getUTCMonth();
-        let userBirthDay = this.DateOfBirth.getUTCDay();
-
-
-        if(userBirthMonth < correctMonth){
-            return correctYear - (userBirthYear);
+//2 task
+function fizzbuzzFunction(n){
+    for(let i = 0; i < n; i++){
+        if(i % 3 === 0 && i % 5 !== 0){
+            console.log('fizz ');
         }
-
-        if(userBirthMonth > correctMonth){
-            return correctYear - (++userBirthYear);
+        else if(i % 5 === 0 && i % 3 !== 0){
+            console.log('buzz ');
         }
-
-        if(userBirthMonth === correctMonth){
-            if(userBirthDay <= correctDay){
-                return correctYear - (userBirthYear);
-            }
-            else if(userBirthDay > correctDay){
-                return correctYear - (++userBirthYear);
-            }
+        else if(i % 3 === 0 && i % 5 === 0){
+            console.log('fizzbuzz');
+        }
+        else{
+            console.log(i);
         }
     }
 }
 
+//3 task
+function avg(...args) {
+  return args.reduce((sum, value) => sum + value) / args.length;
+}
 
-console.log('user salary:', user.salory);
-console.log('user age:', user.age);
+
+//task4
+function makeCounter() {
+  let counterState = 0;
+
+  return function counterAdd() {
+    return ++counterState;
+  };
+}
+
+
+//5 task
+function operation(num1, num2, fn) {
+  return fn(num1, num2);
+}
+
+//6 task
+const obj = {
+  method1() {
+    return this;
+  },
+  method2() {
+    return this;
+  },
+  method3() {
+    return "method 3";
+  },
+};
+
+obj.method1().method2().method3();
+
